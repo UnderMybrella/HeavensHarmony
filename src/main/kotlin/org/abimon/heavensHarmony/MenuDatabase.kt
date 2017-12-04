21189403d257b27abdd6456a4dbc8aba7e4347d0
Isaac <undermybrella@abimon.org>
2017-12-05 10:05:59 +1100

    Did the wrong bot check

--- src/main/kotlin/org/abimon/heavensHarmony/MenuDatabase.kt
+++ src/main/kotlin/org/abimon/heavensHarmony/MenuDatabase.kt
@@ -10,13 +10,13 @@ object MenuDatabase {
     val menus = HashMap<Long, Map<String, MenuOperation>>()
 
     val reactionAdded = DiscordCommand<ReactionAddEvent> { event ->
-        if(!event.user.isBot)
-                return@DiscordCommand
+        if (event.user.isBot)
+            return@DiscordCommand
         val operations = menus[event.message.longID] ?: return@DiscordCommand
         val operate = operations[event.reaction.emoji.toString()] ?: return@DiscordCommand
         operate(event.message, event.reaction, event.user)
 
-        if(!event.channel.isPrivate && PermissionUtils.hasPermissions(event.channel, event.client.ourUser, Permissions.MANAGE_MESSAGES))
+        if (!event.channel.isPrivate && PermissionUtils.hasPermissions(event.channel, event.client.ourUser, Permissions.MANAGE_MESSAGES))
             event.message.removeReaction(event.user, event.reaction)
     }
 
@@ -25,6 +25,7 @@ object MenuDatabase {
         menus.put(msg.longID, operations.toMap())
         operations.forEach { (reaction) -> msg.react(reaction) }
     }
+
     fun registerMenu(msg: IMessage, operations: Map<String, MenuOperation>) = menus.put(msg.longID, operations)
     fun removeMenu(msg: IMessage) = menus.remove(msg.longID)
 }
