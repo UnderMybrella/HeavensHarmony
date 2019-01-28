e74bc396bdfdc0f39560fbdb2ea76015422783dd
Isaac <undermybrella@abimon.org>
2019-01-29 02:08:26 +1100

    Reactions weren't actually added

--- src/main/kotlin/org/abimon/heavens_harmony/MenuDatabase.kt
+++ src/main/kotlin/org/abimon/heavens_harmony/MenuDatabase.kt
@@ -4,18 +4,18 @@ import discord4j.core.DiscordClient
 import discord4j.core.`object`.entity.Message
 import discord4j.core.`object`.reaction.ReactionEmoji
 import discord4j.core.event.domain.message.ReactionAddEvent
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 object MenuDatabase {
     val menus = HashMap<Long, Map<String, MenuOperation<*>>>()
 
-    fun registerMenu(msg: Message, buttons: List<String>, operation: MenuOperation<*>) = registerMenu(msg, buttons.map { it to operation })
-    fun registerMenu(msg: Message, operations: List<Pair<String, MenuOperation<*>>>) {
-        menus.put(msg.id.asLong(), operations.toMap())
-        operations.forEach { (reaction) -> msg.addReaction(ReactionEmoji.unicode(reaction)) }
+    fun registerMenu(msg: Message, buttons: List<String>, operation: MenuOperation<*>): Flux<Void> = registerMenu(msg, buttons.map { it to operation })
+    fun registerMenu(msg: Message, operations: List<Pair<String, MenuOperation<*>>>): Flux<Void> = registerMenu(msg, operations.toMap())
+    fun registerMenu(msg: Message, operations: Map<String, MenuOperation<*>>): Flux<Void> {
+        menus[msg.id.asLong()] = operations
+        return Flux.fromArray(operations.keys.toTypedArray()).flatMap { reaction -> msg.addReaction(ReactionEmoji.unicode(reaction)) }
     }
-
-    fun registerMenu(msg: Message, operations: Map<String, MenuOperation<*>>) = menus.put(msg.id.asLong(), operations)
     fun removeMenu(msg: Message) = menus.remove(msg.id.asLong())
 
     fun register(client: DiscordClient) {
