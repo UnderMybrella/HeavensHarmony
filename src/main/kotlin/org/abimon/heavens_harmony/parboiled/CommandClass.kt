6be88c239c99a54d853c2294269f4b5f83ec17ca
Isaac <undermybrella@abimon.org>
2019-05-14 00:41:10 +1000

    Add makeRuleWith

--- src/main/kotlin/org/abimon/heavens_harmony/parboiled/CommandClass.kt
+++ src/main/kotlin/org/abimon/heavens_harmony/parboiled/CommandClass.kt
@@ -1,13 +1,26 @@
 package org.abimon.heavens_harmony.parboiled
 
 import org.abimon.heavens_harmony.HeavensBot
+import org.parboiled.Action
 import org.parboiled.Rule
+import org.parboiled.support.Var
 
 interface CommandClass {
     val bot: HeavensBot
 
     fun <T : Rule> makeRule(op: HeavensParser.() -> T): T {
         return bot.parser.op()
+    }
+
+    fun <T : Rule, V : Any> makeRuleWith(default: () -> V, op: HeavensParser.(Var<V>) -> T): Rule {
+        return makeRule {
+            val ruleVar = Var<V>()
+            Sequence(
+                    Action<Any> { ruleVar.set(default()) },
+                    bot.parser.op(ruleVar),
+                    Action<Any> { push(ruleVar.get()) }
+            )
+        }
     }
 
 //    fun ParboiledSoldier(rule: Rule, scope: String? = null, command: ParboiledSoldier.(List<Any>) -> Boolean): ParboiledSoldier = ParboiledSoldier(rule, scope, bot, command)
